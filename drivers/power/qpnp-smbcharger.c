f5131ed02c39a0199bc79806c4bdf6976e41dd56
LuizPauloDamaceno <luizpaulodamaceno@live.com>
2019-03-02 00:39:22 -0300

    Regulator / Charger
    -> Incrase voltages to improve stability at higher freq levels;
    -> Reduce charging current a bit to reduce charger (mains side) heating

--- drivers/power/qpnp-smbcharger.c
+++ drivers/power/qpnp-smbcharger.c
@@ -454,7 +454,7 @@ module_param_named(
 	int, S_IRUSR | S_IWUSR
 );
 
-int smbchg_default_dcp_icl_ma = 2000;
+int smbchg_default_dcp_icl_ma = 2100;
 module_param_named(
 	default_dcp_icl_ma, smbchg_default_dcp_icl_ma,
 	int, S_IRUSR | S_IWUSR
