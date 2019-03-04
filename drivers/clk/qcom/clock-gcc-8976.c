820a25eae20ecf0f078f98e7564df1cfa821283c
LuizPauloDamaceno <luizpaulodamaceno@live.com>
2019-03-04 19:42:09 -0300

    GPU/MDP/Regulators: tune limits
    -> incrase a little bit more GPU voltage
    -> incrase a little bit more GPU clock
    -> incrase a little bit more MDP clock
    -> incrase a little bit more CPU voltage improving the stability

--- drivers/clk/qcom/clock-gcc-8976.c
+++ drivers/clk/qcom/clock-gcc-8976.c
@@ -159,7 +159,7 @@ static struct pll_vote_clk gpll3_clk_src = {
 	.status_mask = BIT(17),
 	.base = &virt_bases[GCC_BASE],
 	.c = {
-		.rate = 1100000000,
+		.rate = 1150000000,
 		.parent = &xo_clk_src.c,
 		.dbg_name = "gpll3_clk_src",
 		.ops = &clk_ops_pll_vote,
@@ -1182,7 +1182,7 @@ static struct clk_freq_tbl ftbl_mdp_clk_src[] = {
 	F( 200000000,  gpll0_out_mdp,    4,    0,     0),
 	F( 270000000,      gpll6_out,    4,    0,     0),
 	F( 320000000,  gpll0_out_mdp,  2.5,    0,     0),
-	F( 380000000,      gpll6_out,    3,    0,     0),
+	F( 400000000,      gpll6_out,    3,    0,     0),
 	F_END
 };
 
@@ -1196,7 +1196,7 @@ static struct rcg_clk mdp_clk_src = {
 		.dbg_name = "mdp_clk_src",
 		.ops = &clk_ops_rcg,
 		VDD_DIG_FMAX_MAP4(LOWER, 177780000, LOW, 270000000,
-		NOMINAL, 320000000, HIGH, 380000000),
+		NOMINAL, 320000000, HIGH, 400000000),
 		CLK_INIT(mdp_clk_src.c),
 	},
 };
@@ -1314,7 +1314,7 @@ static struct clk_freq_tbl ftbl_gfx3d_clk_src[] = {
 	F( 480000000,    gpll4_gfx3d,  2.5,    0,     0),
 	F( 550000000,          gpll3,    2,    0,     0),
 	F( 600000000,    gpll4_gfx3d,    2,    0,     0),
-	F( 733333333,          gpll3,  1.5,    0,     0),
+	F( 740000000,          gpll3,  1.5,    0,     0),
 	F_END
 };
 
@@ -1337,7 +1337,7 @@ static struct clk_freq_tbl ftbl_gfx3d_clk_src_v1[] = {
 	F( 550000000,          gpll3,    2,    0,     0),
 	F( 600000000,    gpll4_gfx3d,    2,    0,     0),
 	F( 621330000,    gpll2_gfx3d,  1.5,    0,     0),
-	F( 733333333,          gpll3,  1.5,    0,     0),
+	F( 740000000,          gpll3,  1.5,    0,     0),
 	F_END
 };
 
@@ -2600,7 +2600,7 @@ static struct branch_clk gcc_oxili_gfx3d_clk = {
 		.parent = &gfx3d_clk_src.c,
 		VDD_DIG_FMAX_MAP5(LOWER, 300000000, LOW, 366670000,
 				NOMINAL, 432000000, NOM_PLUS, 480000000,
-				HIGH, 733333333),
+				HIGH, 740000000),
 		.ops = &clk_ops_branch,
 		CLK_INIT(gcc_oxili_gfx3d_clk.c),
 	},
@@ -4057,7 +4057,7 @@ static int msm_gcc_gfx_probe(struct platform_device *pdev)
 
 	if (version) {
 		gfx3d_clk_src.freq_tbl = ftbl_gfx3d_clk_src_v1;
-		gcc_oxili_gfx3d_clk.c.fmax[VDD_DIG_HIGH] = 733333333;
+		gcc_oxili_gfx3d_clk.c.fmax[VDD_DIG_HIGH] = 740000000;
 	}
 
 	ret = of_msm_clock_register(pdev->dev.of_node, msm_clocks_gcc_gfx,
