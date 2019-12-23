055eb4a3b542a3c358b46a4a0a3056be7f8d6271
LuizPauloDamaceno <luizpaulodamaceno@live.com>
2019-12-23 12:08:31 -0300

    take back my 700 MHz for GPU

--- drivers/clk/qcom/clock-gcc-8976.c
+++ drivers/clk/qcom/clock-gcc-8976.c
@@ -1317,7 +1317,7 @@ static struct clk_freq_tbl ftbl_gfx3d_clk_src[] = {
 	F( 550000000,          gpll3,    2,    0,     0),
 	F( 600000000,    gpll4_gfx3d,    2,    0,     0),
 	F( 621330000,    gpll2_gfx3d,  1.5,    0,     0),
-	F( 660000000,	       gpll3,  1.5,    0,     0),
+	F( 700000000,	       gpll3,  1.5,    0,     0),
 	F_END
 };
 
@@ -1340,7 +1340,7 @@ static struct clk_freq_tbl ftbl_gfx3d_clk_src_v1[] = {
 	F( 550000000,          gpll3,    2,    0,     0),
 	F( 600000000,    gpll4_gfx3d,    2,    0,     0),
 	F( 621330000,    gpll2_gfx3d,  1.5,    0,     0),
-	F( 660000000,          gpll3,  1.5,    0,     0),
+	F( 700000000,          gpll3,  1.5,    0,     0),
 	F_END
 };
 
@@ -2605,7 +2605,7 @@ static struct branch_clk gcc_oxili_gfx3d_clk = {
 		.parent = &gfx3d_clk_src.c,
 		VDD_DIG_FMAX_MAP5(LOWER, 300000000, LOW, 366670000,
 				NOMINAL, 432000000, NOM_PLUS, 480000000,
-				HIGH, 660000000),
+				HIGH, 700000000),
 		.flags = CLKFLAG_NO_RATE_CACHE,
 		.ops = &clk_ops_branch,
 		CLK_INIT(gcc_oxili_gfx3d_clk.c),
@@ -4066,7 +4066,7 @@ static int msm_gcc_gfx_probe(struct platform_device *pdev)
 
 	if (version) {
 		gfx3d_clk_src.freq_tbl = ftbl_gfx3d_clk_src_v1;
-		gcc_oxili_gfx3d_clk.c.fmax[VDD_DIG_HIGH] = 660000000;
+		gcc_oxili_gfx3d_clk.c.fmax[VDD_DIG_HIGH] = 700000000;
 	}
 
 	ret = of_msm_clock_register(pdev->dev.of_node, msm_clocks_gcc_gfx,
